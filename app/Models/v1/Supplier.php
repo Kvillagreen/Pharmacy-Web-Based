64ba749de1bafc821d71c12290400953f2dd5f47
Karl Villaverde <karlmacariolavillaverde@gmail.com>
2026-04-11 18:02:02 +0800

    Added Restriction on viewing all branches, updated permission state, fix bugs on login time

--- app/Models/v1/Supplier.php
+++ app/Models/v1/Supplier.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
+    /** @use HasFactory<\Database\Factories\V1\SupplierFactory> */
     use HasFactory;
 
     protected $primaryKey = 'supplier_id'; // important
